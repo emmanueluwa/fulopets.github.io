cb794f172ca6cad3cfe33f5bff6eba283c80bdb2
emmanueluwa <emmanuel@fulodev.com>
2021-12-10 06:19:59 +0000

    adding 2 out of 3 animals pages

--- app.js
+++ app.js
@@ -6,32 +6,25 @@ const dropDownMenuLinks = document.querySelector('.navbar__menu')
 dropDownMenu.addEventListener('click', function() {
   dropDownMenu.classList.toggle('is-active')
   dropDownMenuLinks.classList.toggle('active')
-})
-
-// gallery animation 
-
-const modal = document.querySelector(".modal");
-const imagePreviews = document.querySelectorAll(".gallery img")
-const imageOriginal = document.querySelector(".full__image")
-const caption = document.querySelector(".caption")
-
-imagePreviews.forEach((preview) => {
-    preview.addEventListener("click", () => {
-        modal.classList.add("open");
-        imageOriginal.classList.add("open");
-
-        const originalSrc = preview.getAttribute("data-original");
-        imageOriginal.src = `/images/full/${originalSrc}`;
-
-        const altText = preview.alt;
-        caption.textContent = altText;
-    });
-})
-
-modal.addEventListener('click', (e) => {
-   if (e.target.classList.contains("modal")) {
-     modal.classList.remove("open");
-     imageOriginal.classList.add("open");
-
-   }
 });
+
+
+
+// SLIDE SHOW ANIMATION
+function imageGallery(){
+  const highlight = document.querySelector('.gallery__highlight');
+  const previews = document.querySelectorAll('.room__preview img');
+
+  previews.forEach(preview => {
+    preview.addEventListener('click', function() {
+        const smallSrc = this.src
+        const bigSrc = smallSrc.replace('preview','full')
+        highlight.src = bigSrc;
+        previews.forEach(preview => preview.classList.remove('room__active'));
+        preview.classList.add('room__active');
+      });
+  });
+}
+
+imageGallery();
+
